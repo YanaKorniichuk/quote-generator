3bb045ce72c8b3bbdbd260cd209dee01336054ac
YanaKorniichuk <korniychuk.yana@gmail.com>
2025-07-13 13:23:08 +0300

    added js + typewritter

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["generating the quote"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteForm = document.querySelector("#quote-form");
+quoteForm.addEventListener("submit", generateQuote);
