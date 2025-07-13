65122e0225c4d2c21970e15f7651d778b8ecf8d5
YanaKorniichuk <korniychuk.yana@gmail.com>
2025-07-13 15:56:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ function generateQuote(event) {
 
   let quoteInstructions = document.querySelector("#quote-instructions");
   let apiKey = "b34226b304tb91d11415bf1aef0ee4bo";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
   let context =
     "You are very knowledgeable AI Assistant that knows a lot of quotes of successfull people, your task is to give short quote about topic from the user instructions";
   let prompt = `generate the quote about ${quoteInstructions.value}`;
